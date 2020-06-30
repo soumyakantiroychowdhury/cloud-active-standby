b6e4f12a8dbced94b4afad28373905eae27dbb47
soumyakantiroychowdhury <soumyakanti.roychowdhury@gmail.com>
2020-06-30 16:43:39 +0530

    bi-directional communication

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cloud-active-standby
+
+go 1.13
+
+require github.com/pborman/getopt v0.0.0-20190409184431-ee0cd42419d3
--- server/server.go
+++ server/server.go
@@ -1,16 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/pborman/getopt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "hello world\n")
+	log.Println("Request from " + req.RemoteAddr + " for /hello")
+	fmt.Fprintf(w, "hello world\n")
 }
 
 func main() {
+	var (
+		peerAddr = getopt.StringLong("peerAddr", 'a', "", "IP address or FQDN of the peer instance")
+		peerPort = getopt.StringLong("peerPort", 'p', "", "Port of the peer instance")
+	)
+	log.Println("Starting server at " + time.Now().String())
+	signalch := make(chan os.Signal, 1)
+	signal.Notify(signalch, os.Interrupt, syscall.SIGTERM)
+	ticker := time.NewTicker(1000 * time.Millisecond)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+                fmt.Println("Requesting peer at ", t)
+                http.Get("http://" + *peerAddr + ":" + *peerPort + "/hello")
+			}
+		}
+	}()
+	http.HandleFunc("/hello", hello)
+	go func() {
+		err := http.ListenAndServe(":8090", nil)
+		if err != http.ErrServerClosed {
 
-    http.HandleFunc("/hello", hello)
-    http.ListenAndServe(":8090", nil)
+		}
+	}()
+	<-signalch
+	log.Println("Stopping server")
+	ticker.Stop()
+	done <- true
 }
