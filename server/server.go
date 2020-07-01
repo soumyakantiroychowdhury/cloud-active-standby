5f2e4c61a99c96b25695d48358dda4efde163d39
soumyakantiroychowdhury <soumyakanti.roychowdhury@gmail.com>
2020-07-01 06:40:31 +0530

    Fix getOpt issue

--- server/server.go
+++ server/server.go
@@ -20,7 +20,14 @@ func main() {
 	var (
 		peerAddr = getopt.StringLong("peerAddr", 'a', "", "IP address or FQDN of the peer instance")
 		peerPort = getopt.StringLong("peerPort", 'p', "", "Port of the peer instance")
+		help     = getopt.BoolLong("help", 'h', "Help")
 	)
+	getopt.Parse()
+	if *help {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
 	log.Println("Starting server at " + time.Now().String())
 	signalch := make(chan os.Signal, 1)
 	signal.Notify(signalch, os.Interrupt, syscall.SIGTERM)
@@ -31,12 +38,12 @@ func main() {
 			select {
 			case <-done:
 				return
-			case t := <-ticker.C:
-                fmt.Println("Requesting peer at ", t)
-                res, err := http.Get("http://" + *peerAddr + ":" + *peerPort + "/hello")
-                if err == nil {
-                    res.Body.Close()
-                }
+			case <-ticker.C:
+				peerURL := "http://" + *peerAddr + ":" + *peerPort + "/hello"
+				res, err := http.Get(peerURL)
+				if err == nil {
+					res.Body.Close()
+				}
 			}
 		}
 	}()
@@ -44,7 +51,6 @@ func main() {
 	go func() {
 		err := http.ListenAndServe(":8090", nil)
 		if err != http.ErrServerClosed {
-
 		}
 	}()
 	<-signalch
